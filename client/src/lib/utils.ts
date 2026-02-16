0e563c79a85b3bb9810534169b4e394d18e0872c
Ahmad Othman <ahmad.ali.othman@outlook.com>
2026-02-16 22:06:21 +0200

    feat: add version comparison utility
    - export `compareVersions` function
    - improve path aliases for imports
    - update HomeView to display file existence

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,7 +1,16 @@
 import type { ClassValue } from "clsx";
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import type { Version } from "@/types";
 
-export function cn(...inputs: ClassValue[]) {
+function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+function compareVersions(a: Version, b: Version): number {
+  if (a.major !== b.major) return a.major - b.major;
+  if (a.minor !== b.minor) return a.minor - b.minor;
+  return a.patch - b.patch;
+}
+
+export { cn, compareVersions };
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { computedAsync } from "@vueuse/core";
+import { fs } from "@/services/filesystem";
 
-onMounted(async () => {});
+const fileInfo = computedAsync(async () => {
+  return await fs.exists("package.json");
+});
 </script>
 
 <template>
   <main>
     <pre>
-      {{   }}
+      file info: {{ fileInfo?.ok }}
     </pre>
   </main>
 </template>
