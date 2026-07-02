2ac8dbaef3667e8ec5acf18fdd91f8c71b8075a6
Ahmad Othman <ahmad.ali.othman@outlook.com>
2026-07-02 18:07:52 +0300

    Updated Files 2026-07-02@06:07pm

--- client/src/route-map.d.ts
+++ client/src/route-map.d.ts
@@ -33,13 +33,6 @@ declare module 'vue-router/auto-routes' {
       Record<never, never>,
       | never
     >,
-    '/about': RouteRecordInfo<
-      '/about',
-      '/about',
-      Record<never, never>,
-      Record<never, never>,
-      | never
-    >,
     '/test': RouteRecordInfo<
       '/test',
       '/test',
@@ -63,12 +56,6 @@ declare module 'vue-router/auto-routes' {
     'src/pages/index.vue': {
       routes:
         | '/'
-      views:
-        | never
-    }
-    'src/pages/about.vue': {
-      routes:
-        | '/about'
       views:
         | never
     }
